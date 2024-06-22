e626bd3ef26109f3b8603d032cc46b2f8e28fcf5
alladishashank <alladishashank@gmail.com>
2024-06-22 10:27:29 +0530

    Added edit assessment feature

--- src/app/view-assessment/view-assessment.component.ts
+++ src/app/view-assessment/view-assessment.component.ts
@@ -4,19 +4,21 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-assessment',
   templateUrl: './view-assessment.component.html',
   styleUrls: ['./view-assessment.component.css'],
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule]
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, FormsModule]
 })
 export class ViewAssessmentComponent implements OnInit {
   assessmentId: string | null = null;
   assessmentDetails: any = null;
   groupedScores: any[] = [];
+  isEditMode: boolean = false;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -64,5 +66,27 @@ export class ViewAssessmentComponent implements OnInit {
     }
   }
 
-  
+  getTooltipText(scoringLevel: string): string {
+    switch (scoringLevel) {
+      case 'Navigator':
+        return 'AMA Score is 5 or below';
+      case 'Voyager':
+        return 'AMA Score up to 8';
+      case 'Visionary':
+        return 'AMA Score above 8';
+      default:
+        return '';
+    }
+  }
+
+  onEdit(): void {
+    console.log('Edit button clicked');
+    this.isEditMode = true;
+  }
+
+  onSave(): void {
+    console.log('Save button clicked');
+    console.log(this.assessmentDetails);
+    this.isEditMode = false;
+  }
 }
